9585266c5037529da5c254f95c054296770fdbcb
Cristian Henrique <cristianhenrique06@hotmail.com>
2018-05-16 15:06:39 -0300

    Atualização: utilizando somente JsonIgnore

--- src/main/java/com/cristianhenrique/cursomc/domain/Cidade.java
+++ src/main/java/com/cristianhenrique/cursomc/domain/Cidade.java
@@ -9,8 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Cidade implements Serializable{
 	
@@ -20,7 +19,9 @@ public class Cidade implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-	@JsonManagedReference
+	
+	
+	
 	@ManyToOne
 	@JoinColumn(name="estado_id")
 	private Estado estado;
--- src/main/java/com/cristianhenrique/cursomc/domain/Estado.java
+++ src/main/java/com/cristianhenrique/cursomc/domain/Estado.java
@@ -10,8 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Estado implements Serializable {
@@ -21,7 +20,9 @@ public class Estado implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-	@JsonBackReference
+	
+	
+	@JsonIgnore
 	@OneToMany(mappedBy="estado")
 	private List<Cidade> cidades = new ArrayList<>();
 	
