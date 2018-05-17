99b43f559c5c172fe35a8e9127204e5ceab9c03a
Cristian Henrique <cristianhenrique06@hotmail.com>
2018-05-17 14:58:21 -0300

    Listando todas Categorias com DTO

--- src/main/java/com/cristianhenrique/cursomc/services/CategoriaService.java
+++ src/main/java/com/cristianhenrique/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.cristianhenrique.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,5 +46,11 @@ public class CategoriaService {
 		}catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
+	}
+	//padrão DTO Data Trasnferin object OBJETO DE TRANSFERENCIA DE DADOS
+	public List<Categoria> findAll() {
+		
+		return repo.findAll();
+		
 	}
 }
