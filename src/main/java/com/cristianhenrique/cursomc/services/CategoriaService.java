39bda02a9171fee5db02a6646dc2997fb64d39a3
Cristian Henrique <cristianhenrique06@hotmail.com>
2018-05-21 23:51:03 -0300

    Correções: update em CategoriaService e contrutor de Pagamento

--- src/main/java/com/cristianhenrique/cursomc/services/CategoriaService.java
+++ src/main/java/com/cristianhenrique/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.cristianhenrique.cursomc.domain.Categoria;
+import com.cristianhenrique.cursomc.domain.Categoria;
 import com.cristianhenrique.cursomc.dto.CategoriaDTO;
 import com.cristianhenrique.cursomc.repositories.CategoriaRepository;
 import com.cristianhenrique.cursomc.services.exceptions.DataIntegrityException;
@@ -39,9 +40,13 @@ public class CategoriaService {
 		return repo.save(obj);
 	}
 	public Categoria update (Categoria obj) {
-		 find(obj.getId());
-		return repo.save(obj);
+		Categoria  newObj = find(obj.getId());
+		updateData(newObj, obj);
+		
+		return repo.save(newObj);
 	}
+
+	
 
 	public void delete(Integer id) {
 		find(id);
@@ -66,6 +71,12 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());   
+	}
+	
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+		
 	}
 	
 }
