8722499bdf8e8b4919ad3df12a994f24446e89cb
Cristian Henrique <cristianhenrique06@hotmail.com>
2018-05-25 19:06:57 -0300

    Validação customizada: email não repetido na inserção de cliente

--- src/main/java/com/cristianhenrique/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/cristianhenrique/cursomc/repositories/ClienteRepository.java
@@ -3,10 +3,13 @@ package com.cristianhenrique.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cristianhenrique.cursomc.domain.Cliente;
 //Classe que será capaz de acessar o banco de dados e fazer as consultas nescessárias da tabela cateoria
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
